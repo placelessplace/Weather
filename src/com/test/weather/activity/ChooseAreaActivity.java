5f27ce7a5c8882dccf7d6a8a466399026c7bd109
zhujing <603406797@qq.com>
2015-10-02 17:35:12 +0800

    新增切换城市和手动更新天气的功能

--- src/com/test/weather/activity/ChooseAreaActivity.java
+++ src/com/test/weather/activity/ChooseAreaActivity.java
@@ -61,11 +61,15 @@ public class ChooseAreaActivity extends Activity{
 	//当前选中的级别
 	private int currentLevel;
 
+	//是否从WeatherActivity中跳转过来
+	private boolean isFromWeatherActivity;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if(prefs.getBoolean("city_selected", false)){
+		//已经选择了城市而且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if(prefs.getBoolean("city_selected", false)&&!isFromWeatherActivity){
 			Intent intent = new Intent(this,WeatherActivity.class);
 			startActivity(intent);
 			finish();
@@ -112,7 +116,7 @@ public class ChooseAreaActivity extends Activity{
 			}
 			adapter.notifyDataSetChanged();
 			listView.setSelection(0);
-			titleText.setText("中国");
+			titleText.setText("中国天气查询");
 			currentLevel = LEVEL_PROVINCE;
 		}else{
 			queryFromServer(null,"province");
@@ -233,6 +237,10 @@ public class ChooseAreaActivity extends Activity{
 		}else if (currentLevel == LEVEL_COUNTY){
 			queryProvinces();
 		}else {
+			if(isFromWeatherActivity){
+				Intent intent = new Intent(this,WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 			
